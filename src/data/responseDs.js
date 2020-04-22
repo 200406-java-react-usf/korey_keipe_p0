036dccb2332b629a46cb0b14e092dc55c63f8d09
khkeipe <kkeipe@gmail.com>
2020-04-21 19:29:09 -0500

    adding basic temp response data source

---
+++ src/data/responseDs.js
@@ -0,0 +1,8 @@
+const response = require('../models/user');
+id = 0;
+
+module.exports = responseData = [
+
+    new user(id++, 'this is a test response', 'https://www.adventuresquadcreations.com/'),
+
+]
