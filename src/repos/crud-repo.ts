4c3de0e47ee405b52486957817ccc65b4f42d927
khkeipe <kkeipe@gmail.com>
2020-04-22 14:50:29 -0500

    basic crud repo strucutre

---
+++ src/repos/crud-repo.ts
@@ -0,0 +1,9 @@
+export interface CrudRepository<T> {
+
+    getAll(): Promise<T>;
+    getById(id: number): Promise<T>;
+    save(newObj: T): Promise<T>;
+    update(updateOjb: T): Promise<boolean>;
+    deleteById(id: number): Promise<boolean>;
+
+}
