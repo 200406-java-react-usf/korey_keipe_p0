d5b6da7e4f0d8dfa48665595b0934c7c660a83a1
khkeipe <kkeipe@gmail.com>
2020-04-23 09:28:54 -0500

    linking style fixes in userDs.js and added lint dependancies

--- src/data/userDs.ts
+++ src/data/userDs.ts
@@ -3,8 +3,8 @@ let id: number = 0;
 
 export default[
 
-    new User(id++, 'test1', 'password', 'test1@email.com'),
-    new User(id++, 'test2', 'password', 'test2@email.com'),
-    new User(id++, 'test3', 'password', 'test3@email.com'),
+	new User(id++, 'test1', 'password', 'test1@email.com'),
+	new User(id++, 'test2', 'password', 'test2@email.com'),
+	new User(id++, 'test3', 'password', 'test3@email.com'),
 
-]
+];
