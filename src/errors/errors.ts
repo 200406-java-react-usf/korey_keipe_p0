281f7c88851af3f7c590516fa39fe06e3749c57a
khkeipe <kkeipe@gmail.com>
2020-04-22 20:09:43 -0500

    created error.ts file with error classes

---
+++ src/errors/errors.ts
@@ -0,0 +1,42 @@
+class AppError {
+
+    message: string;
+    reason: string;
+
+    constructor(reason?: string){
+        this.message = 'An unexpected error has occured.';
+        reason ? (this.reason = reason) : this.reason = 'Unknown reason'
+    }
+
+    setMessage(message: string){
+        this.message = message;
+    }
+}
+
+class DataNotFoundError extends AppError{
+    constructor(reason?: string){
+        super(reason);
+        super.setMessage('Error: No data was found')
+    }
+}
+
+class InvalidRequestError extends AppError{
+    constructor(reason?: string){
+        super(reason);
+        super.setMessage('Error: Invalid Request')
+    }
+}
+
+class AuthenticationError extends AppError{
+    constructor(reason?: string){
+        super(reason);
+        super.setMessage('Error: Authentication Failed')
+    }
+}
+
+class DataNotStoredError extends AppError{
+    constructor(reason?: string){
+        super(reason);
+        super.setMessage('Error: The data has not been stored')
+    }
+}
