899732984db17e730bda19f886c5f681a0f8b68a
khkeipe <kkeipe@gmail.com>
2020-04-25 15:22:47 -0500

    formating changes to remove white space

--- src/data/userDs.ts
+++ src/data/userDs.ts
@@ -2,7 +2,7 @@ import { User } from '../models/user';
 let id: number = 1;
 
 export default[
-	new User(id++, 'ClydesCreations', 'password', 'yoopertrooper906@email.com'),
-	new User(id++, 'KoreyKeipe', 'password', 'kkeipe@email.com'),
+	new User(id++, 'ClydesCreations', 'password', 'yoopertrooper906@gmail.com'),
+	new User(id++, 'KoreyKeipe', 'password', 'kkeipe@gmail.com'),
 	new User(id++, 'ASC', 'password', 'korey.keipe@dreamcatcherllc.us'),
 ];
