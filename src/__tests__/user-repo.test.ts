7124152d428eb18ce57253f57cb8cb946fff3c18
khkeipe <kkeipe@gmail.com>
2020-04-25 13:12:25 -0500

    adding test to valide empty data base for getAll method

--- src/__tests__/user-repo.test.ts
+++ src/__tests__/user-repo.test.ts
@@ -1,4 +1,5 @@
 import { UserRepository as sut } from '../repos/user-Repo';
+import { DataNotFoundError } from '../errors/errors';
 
 describe('User Repo',()=>{
 
@@ -6,13 +7,23 @@ describe('User Repo',()=>{
 
 		// Arrange
 		expect.assertions(1);
-
 		// Act
 		let result = await sut.getInstance().getAll();
-
 		// Accert
 		expect(result).toBeTruthy();
 
 	});
 
+	test('should throw DataNotFoundError when invoking getAll and the data base is empty', async () => {
+
+		// Arrange
+		expect.assertions(0);
+		// Act
+		try{
+			await sut.getInstance().getAll();
+		} catch (e){
+		// Accert
+			expect(e).toThrow(DataNotFoundError);
+		}
+	});
 });
