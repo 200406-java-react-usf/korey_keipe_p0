62833fe3e6ff2e34e814e12f5b39825fc8ce291b
khkeipe <kkeipe@gmail.com>
2020-04-25 21:01:40 -0500

    use routers to output commands and response to browser

--- src/app.ts
+++ src/app.ts
@@ -3,29 +3,22 @@ import { UserRepository } from './repos/user-Repo';
 import { CommandRepository } from './repos/command-repo';
 import { ResponseRepository } from './repos/response-repo';
 import { UserRouter } from './routes/user-router';
+import { ResponseRouter } from './routes/response-router';
+import { CommandRouter } from './routes/command-router';
 import bodyParser from 'body-parser';
+
 
 const app = express();
 const port = 8080;
 
-let userRepo = UserRepository.getInstance();
-let commandRepo = CommandRepository.getInstance();
-let responseRepo = ResponseRepository.getInstance();
-
 app.use('/', bodyParser.json());
 
 app.use('/users', UserRouter);
+app.use('/commands', CommandRouter);
+app.use('/responses', ResponseRouter);
 
-app.get('/commands', async (req, res)=>{
-	let payload = await commandRepo.getAll();	
-	res.status(200).send(payload);
-});
 
-app.get('/responses', async (req, res)=>{
-	let payload = await responseRepo.getAll();	
-	res.status(200).send(payload);
-});
-
+// Must remain at bottom
 app.listen(port, ()=>{
 	console.log(`Server is now running on ${port}`);
 });
