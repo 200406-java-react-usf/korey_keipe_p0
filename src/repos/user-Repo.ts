a8c5449283223a048d83df4b1b2e994fb7276581
khkeipe <kkeipe@gmail.com>
2020-04-26 18:14:12 -0500

    implement save method in user-repo

--- src/repos/user-Repo.ts
+++ src/repos/user-Repo.ts
@@ -4,6 +4,7 @@ import { User } from '../models/user';
 import { 
 	DataNotFoundError,
 	InvalidRequestError,
+	DataNotStoredError,
 } from '../errors/errors';
 import { validateId } from '../util/validation';
 import { validateObj }from '../util/validation';
@@ -88,8 +89,22 @@ export class UserRepository implements CrudRepository<User> {
 	save(newUser: User): Promise<User>{
 
 		return new Promise((resolve, reject) => {
+			
+			if(!validateObj(newUser, 'id')){
+				reject(new InvalidRequestError('Invalid User'));
+				return;
+			}
 
+			for(let user of data){
+				if(newUser.username === user.username || newUser.email === user.email){
+					reject(new InvalidRequestError('Username is already in use'));
+					return;
+				}
+			}
 
+			newUser.id = data.length + 1;
+			data.push(newUser);
+			resolve(this.passwordHide(newUser));
 
 		});
 	}
