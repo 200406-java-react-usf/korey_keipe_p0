4fa5e910737874faa13836b5404f553d1a6c6e35
khkeipe <kkeipe@gmail.com>
2020-04-26 18:13:10 -0500

    fix white space in userDs

--- src/data/userDs.ts
+++ src/data/userDs.ts
@@ -1,7 +1,7 @@
 import { User } from '../models/user';
 let id: number = 1;
 
-export default[
+export default [
 	new User(id++, 'ClydesCreations', 'password', 'yoopertrooper906@gmail.com'),
 	new User(id++, 'KoreyKeipe', 'password', 'kkeipe@gmail.com'),
 	new User(id++, 'ASC', 'password', 'korey.keipe@dreamcatcherllc.us'),
