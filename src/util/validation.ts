a71fc0ccd5e80d72572327097ce0f5c2d2be27aa
khkeipe <kkeipe@gmail.com>
2020-04-25 20:38:44 -0500

    adding validation methods

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,18 +1,21 @@
-import { UserRepository } from '../repos/user-Repo';
 
+const validateId = (id: number): boolean =>{
+	return (id && typeof id === 'number' && Number.isInteger(id) && id > 0);
+};
 
-class validateId {
+const validateString = (...string: string[]): boolean => {
+	return (string.filter(string => !string || typeof string !== 'string').length == 0);
+};
 
+const validateObj = (obj: Object, ...nullProperties: string[]) =>{
+	return obj && Object.keys(obj).every(key => {
+		if(key ==  'id') return validateId(obj['id']);
+		if(nullProperties.includes(key)) return true;
+	});
+};
 
-
-}
-
-class validateString {
-
-}
-
-export {
+export default {
 	validateId,
 	validateString,
-
+	validateObj
 };
