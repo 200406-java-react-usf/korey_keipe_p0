9cfb3ba95103b2f9ad8d72df8d9137bb3c6cd94c
khkeipe <kkeipe@gmail.com>
2020-04-29 11:38:48 -0500

    commenting out command and response routers until user is functional

--- src/routes/command-router.ts
+++ src/routes/command-router.ts
@@ -1,25 +1,26 @@
 import express from 'express';
 import { CommandRepository } from '../repos/command-repo';
+import Appconfig from '../config/app';
 
 export const CommandRouter = express.Router();
 
-const commandRepo = CommandRepository.getInstance();
+const commandService = Appconfig.commandService;
 
-CommandRouter.get('/', async (req, res) => {
-	try{
-		let result = await commandRepo.getAll();
-		return res.status(200).json(result);
-	}catch(e){
-		return res.status(404).json(e);
-	}
-});
+// CommandRouter.get('/', async (req, res) => {
+// 	try{
+// 		let result = await commandService.getAllCommands();
+// 		return res.status(200).json(result);
+// 	}catch(e){
+// 		return res.status(404).json(e);
+// 	}
+// });
 
-CommandRouter.get('/:id', async (req, res) => {
-	const id = +req.params.id;
-	try{
-		let result = await commandRepo.getById(id);
-		return res.status(200).json(result);
-	}catch(e){
-		return res.status(404).json(e);
-	}
-});
+// CommandRouter.get('/:id', async (req, res) => {
+// 	const id = +req.params.id;
+// 	try{
+// 		let result = await commandService.getCommandById(id);
+// 		return res.status(200).json(result);
+// 	}catch(e){
+// 		return res.status(404).json(e);
+// 	}
+// });
--- src/routes/response-router.ts
+++ src/routes/response-router.ts
@@ -1,28 +1,29 @@
 import express from 'express';
 import { ResponseRepository } from '../repos/response-repo';
+import Appconfig from '../config/app';
 
 export const ResponseRouter = express.Router();
 
-const responseRepo = ResponseRepository.getInstance();
+const responseService = Appconfig.responseService;
 
-ResponseRouter.get('/', async (req, res) => {
-	try{
-		let result = await responseRepo.getAll();
-		return res.status(200).json(result);
-	}catch(e){
-		return res.status(404).json(e);
-	}
-});
+// ResponseRouter.get('/', async (req, res) => {
+// 	try{
+// 		let result = await responseService.getAllResponses();
+// 		return res.status(200).json(result);
+// 	}catch(e){
+// 		return res.status(404).json(e);
+// 	}
+// });
 
-ResponseRouter.get('/:id', async (req, res) => {
-	const id = +req.params.id;
-	try{
-		let result = await responseRepo.getById(id);
-		return res.status(200).json(result);
-	}catch(e){
-		return res.status(404).json(e);
-	}
-});
+// ResponseRouter.get('/:id', async (req, res) => {
+// 	const id = +req.params.id;
+// 	try{
+// 		let result = await responseService.getResponseById(id);
+// 		return res.status(200).json(result);
+// 	}catch(e){
+// 		return res.status(404).json(e);
+// 	}
+// });
 
 
 
