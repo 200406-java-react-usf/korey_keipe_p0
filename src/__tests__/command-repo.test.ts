f663c4dc007e884163d898ac02f1e494b1aaf91d
khkeipe <kkeipe@gmail.com>
2020-04-29 12:06:21 -0500

    removed tests to be refactored when DB in place

--- src/__tests__/command-repo.test.ts
+++ src/__tests__/command-repo.test.ts
@@ -4,24 +4,5 @@ import { DataNotFoundError
 
 describe('Command Repo', ()=>{
 
-	test('should be truthy commands when getAll method is invoked', async () =>{
 
-		// Arrange
-		expect.assertions(1);
-		// Act
-		let result = await sut.getInstance().getAll();
-		// Assert
-		expect(result).toBeTruthy();
-	});
-
-	test('should throw DataNotFoundError when getAll method is invoked with empty database', async ()=>{
-		// Arrange
-		expect.assertions(1);
-		// Act
-		try{
-			await sut.getInstance().getAll();
-		}catch(e){
-			expect(e instanceof DataNotFoundError).toBeTruthy();
-		}
-	});
 });
--- src/__tests__/response-repo.test.ts
+++ src/__tests__/response-repo.test.ts
@@ -3,21 +3,5 @@ import { DataNotFoundError } from '../errors/errors';
 
 describe('Response Repo', () => {
 
-	test('should be truthy when getAll method is invoked', async () => {
-		// Arrange
-		expect.assertions(1);
-		// Act
-		let result = await sut.getInstance().getAll();
-		// Accert
-		expect(result).toBeTruthy();
-	});
 
-	test('should throw DataNotFoundError when getAll is invoked with no reponses in database',async () => {
-		expect.assertions(1);
-		try{
-			await sut.getInstance().getAll();
-		}catch(e){
-			expect(e instanceof DataNotFoundError).toBeTruthy();
-		}
-	});
 });
