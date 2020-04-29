593ae45f3402c4d216cf3b7af5fa2eb9b6b23fd2
khkeipe <kkeipe@gmail.com>
2020-04-29 10:58:05 -0500

    refactor to use service class

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,26 +1,25 @@
 import express from 'express';
-import { User } from '../models/user';
-import { UserRepository } from '../repos/user-Repo';
+import Appconfig from '../config/app';
 
 export const UserRouter = express.Router();
 
-const userRepo = UserRepository.getInstance();
+const userService = Appconfig.userService;
 
 UserRouter.get('/', async (req, res) => {
 	try{
-		let payload = await userRepo.getAll();
+		let payload = await userService.getAllUsers();
 		return res.status(200).json(payload);
 	}catch (e) {
 		return res.status(404).json(e);
 	}
 });
 
-UserRouter.get('/:id', async (req,res)=>{
-	const id = +req.params.id;
-	try{
-		let payload = await userRepo.getById(id);	
-		return res.status(200).json(payload);
-	}catch(e){
-		return res.status(404).json(e);
-	}
-});
+// UserRouter.get('/:id', async (req,res)=>{
+// 	const id = +req.params.id;
+// 	try{
+// 		let payload = await userService.getUserById(id);	
+// 		return res.status(200).json(payload);
+// 	}catch(e){
+// 		return res.status(404).json(e);
+// 	}
+// });
