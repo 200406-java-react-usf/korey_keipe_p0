35567545cb8419dc2480e66fdc73acbf4c3ffa6d
khkeipe <kkeipe@gmail.com>
2020-05-01 15:10:23 -0500

    implement getUserByKey method

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -5,7 +5,8 @@ import { DataNotFoundError,
 } from '../errors/errors';
 import {validateId,
 	validateObj,
-	validateString
+	validateString,
+	isPropertyOf
 } from '../util/validation';
 
 export class UserService {
@@ -49,6 +50,35 @@ export class UserService {
 		let user = await this.userRepo.save(newUser);
 		return this.passwordHide(user);
 
+	}
+
+	async getUserByKey(queryObj: any): Promise<User> {
+		
+		let queryKeys = Object.keys(queryObj);
+
+		if(!queryKeys.every(key => isPropertyOf(key, User))) {
+			throw new InvalidRequestError();
+		}
+
+		let key = queryKeys[0];
+		let value = queryObj[key];
+
+		if (key === 'id'){
+			return await this.getUserById(+value);
+		}
+
+		if (!validateId(value)){
+			throw new InvalidRequestError();
+		}
+
+		let user = await this.userRepo.getByKey(key, value);
+
+		if (validateObj(user)){
+			throw new DataNotFoundError();
+		}
+
+		return this.passwordHide(user);
+		
 	}
 
 	private passwordHide(user: User){
