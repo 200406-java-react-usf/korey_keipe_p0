80499471c6dc53e5d21dbd99e8bc68d5790b94a9
khkeipe <kkeipe@gmail.com>
2020-05-03 17:35:32 -0500

    adding result set mappers

---
+++ src/util/result-set-map.ts
@@ -0,0 +1,45 @@
+import { UserSchema, CommandSchema, ResponseSchema } from '../util/schema';
+import { User } from '../models/user';
+import { Command } from '../models/command';
+import { Response } from '../models/response';
+
+export function mapUserResultSet(resultSet: UserSchema): User {
+	
+	if(!resultSet){
+		return {} as User;
+	}
+
+	return new User (
+		resultSet.id,
+		resultSet.username,
+		resultSet.password,
+		resultSet.email
+	);
+}
+
+export function mapCommandResultSet(resultSet: CommandSchema): Command {
+
+	if(!resultSet){
+		return {} as Command;
+	}
+
+	return new Command (
+		resultSet.id,
+		resultSet.keyword,
+		resultSet.userId
+	);
+}
+
+export function mapResponseResultSet(resultSet: ResponseSchema): Response {
+
+	if(!resultSet){
+		return {} as Response;
+	}
+
+	return new Response (
+		resultSet.id,
+		resultSet.body,
+		resultSet.link,
+		resultSet.commandId
+	);
+}
