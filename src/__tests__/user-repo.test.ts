2ca6eb32316bedd343846e643566d1ccc6500aed
khkeipe <kkeipe@gmail.com>
2020-05-04 11:20:52 -0500

    full coverage on user-repo tests

--- src/__tests__/user-repo.test.ts
+++ src/__tests__/user-repo.test.ts
@@ -2,6 +2,7 @@ import { UserRepository } from '../repos/user-Repo';
 import * as mockIndex from '..';
 import * as mockMapper from '../util/result-set-map';
 import { User } from '../models/user';
+import { InternalServerError } from '../errors/errors';
 
 jest.mock('..', () => {
 	return {
@@ -102,6 +103,199 @@ describe('User Repo',()=>{
 		expect(result).toBeTruthy();
 		expect(result instanceof User).toBe(true);
 
+	});
+
+	test('should return and newUser when save succesfully persist', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+
+		// Act
+		let result = await sut.save(mockUser);
+
+		// Assert
+		expect(result).toBeTruthy();
+
+	});
+
+	test('should return void when deleteById is succesfully exectued by the database', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		let result = await sut.update(mockUser);
+
+		// Assert
+		expect(result).toBeTruthy();
+
+	});
+
+	test('should return void when update is succesfully exectued by the database', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		let result = await sut.deleteById(1);
+
+		// Assert
+		expect(result).toBeTruthy();
+
+	});
+
+	test('should return and user when getByUsername recieves a specified user from the database',async ()=>{
+
+		// Arrange
+		expect.hasAssertions();
+
+		let mockUser = new User (1, 'un', 'pw', 'email');
+		(mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+
+		// Act
+		let result = await sut.getByUsername('un');
+
+		// Assert
+		expect(result).toBeTruthy();
+		expect(result instanceof User).toBe(true);
+
+	});
+
+	test('should return and user when getByEmail recieves a specified user from the database',async ()=>{
+
+		// Arrange
+		expect.hasAssertions();
+
+		let mockUser = new User (1, 'un', 'pw', 'email');
+		(mockMapper.mapUserResultSet as jest.Mock).mockReturnValue(mockUser);
+
+
+		// Act
+		let result = await sut.getByEmail('email');
+
+		// Assert
+		expect(result).toBeTruthy();
+		expect(result instanceof User).toBe(true);
+
+	});
+
+	test('should throw InternalServerError when save() is envoked but query is unsuccesful', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return false; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		try {
+			await sut.save(mockUser);
+		} catch (e) {
+			// Assert
+			expect(e instanceof InternalServerError).toBe(true);
+		}
+	});
+
+	test('should throw InternalServerError when getAll() is envoked but query is unsuccesful', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return false; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		try {
+			await sut.getAll();
+		} catch (e) {
+			// Assert
+			expect(e instanceof InternalServerError).toBe(true);
+		}
+	});
+
+	test('should throw InternalServerError when getById() is envoked but query is unsuccesful', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return false; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		try {
+			await sut.getById(mockUser.id);
+		} catch (e) {
+			// Assert
+			expect(e instanceof InternalServerError).toBe(true);
+		}
+	});
+
+	test('should throw InternalServerError when getByUsername() is envoked but query is unsuccesful', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return false; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		try {
+			await sut.getByUsername(mockUser.username);
+		} catch (e) {
+			// Assert
+			expect(e instanceof InternalServerError).toBe(true);
+		}
+	});
+
+	test('should throw InternalServerError when getByEmail() is envoked but query is unsuccesful', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		let mockUser = new User (1, 'test', 'password', 'email');
+		(mockConnect as jest.Mock).mockImplementation( () => {
+			return {
+				query: jest.fn().mockImplementation( () => { return false; }),
+				release: jest.fn()
+			};
+		});
+
+		// Act
+		try {
+			await sut.getByEmail(mockUser.email);
+		} catch (e) {
+			// Assert
+			expect(e instanceof InternalServerError).toBe(true);
+		}
 	});
 
 });
