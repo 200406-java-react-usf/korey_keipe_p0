d56371cc790757f8fa396dd9540d1dffb573b34e
khkeipe <kkeipe@gmail.com>
2020-05-04 17:35:42 -0500

    adding documentation to validation.ts

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,13 +1,23 @@
-
-
+/**
+ * Validation of acceptable ID. (must be a non-negative integer value)
+ * @param {string} id - unique key for specific element in the database.
+ */
 export const validateId = (id: number): boolean =>{
 	return (id && typeof id === 'number' && Number.isInteger(id) && id > 0);
 };
 
+/**
+ * Validation of acceptable string. 
+ * @param {string} string - must be a truthy value, and not empty
+ */
 export const validateString = (...string: string[]): boolean => {
 	return (string.filter(string => !string || typeof string !== 'string').length == 0);
 };
 
+/**
+ * Validation of acceptable object. 
+ * @param {string} obj - must be a truthy value where each element is not empty
+ */
 export const validateObj = (obj: Object, ...nullProperties: string[]) =>{
 	return obj && Object.keys(obj).every(key => {
 		if(nullProperties.includes(key)) return true;
@@ -15,12 +25,21 @@ export const validateObj = (obj: Object, ...nullProperties: string[]) =>{
 	});
 };
 
+/**
+ * Validation of an empty object. 
+ * @param {string} obj - must be an object that is not empty
+ */
 export const vaildateEmptyObj = (obj: Object) => {
 	if (Object.keys(obj).length == 0){
 		return true;
 	}
 };
 
+/**
+ * deteremine if a specified proper is of a particular type 
+ * @param {string} prop - property of an object
+ * @param {string} type - data type 
+ */
 export const isPropertyOf = (prop: string, type: any) => {
 	if(!prop || !type){
 		return false;
