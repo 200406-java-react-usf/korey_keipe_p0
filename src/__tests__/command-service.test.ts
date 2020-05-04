16346603e45993462a2b53b885de446d038a6712
khkeipe <kkeipe@gmail.com>
2020-05-03 22:00:48 -0500

    adding tests for getbyid

--- src/__tests__/command-service.test.ts
+++ src/__tests__/command-service.test.ts
@@ -3,6 +3,7 @@ import { CommandRepository } from '../repos/command-repo';
 import { Command } from '../models/command';
 import Validator from '../util/validation';
 import { DataNotFoundError, InvalidRequestError } from '../errors/errors';
+import validation from '../util/validation';
 
 jest.mock('../repos/command-repo', () => {
 
@@ -58,6 +59,126 @@ describe('commandService', () => {
 		// Assert
 		expect(result).toBeTruthy();
 		expect(result.length).toBe(7);
+	});
+
+	test('should throw a DataNotFoundError when getAllCommands fails to get any commands from the database', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getAll = jest.fn().mockReturnValue([]);
+
+		// Act
+		try{
+			await sut.getAllCommands();
+		} catch (e) {
+		// Assert
+			expect(e instanceof DataNotFoundError).toBeTruthy();
+		}
+	});
+
+	test('should resolve a command when getCommandById is given a valid id that is in the database', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+
+		validation.validateId = jest.fn().mockReturnValue(true);
+
+		mockRepo.getById = jest.fn().mockImplementation((id: number) => {
+			return new Promise<Command>((resolve) => resolve(mockCommands[id - 1]));
+		});
+
+		// Act
+		let result = await sut.getCommandById(1);
+
+		// Assert
+		expect(result).toBeTruthy();
+		expect(result.id).toBe(1);
+
+	});
+
+	test('should throw InvalidRequestError when getCommandById is provided a negative id value', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getById = jest.fn().mockReturnValue(false);
+
+		// Act
+		try {
+			await sut.getCommandById(-1);
+		} catch (e) {
+
+			// Assert
+			expect(e instanceof InvalidRequestError).toBe(true);
+		}
+
+	});
+
+	test('should throw InvalidRequestError when getCommandById is given a of zero)', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getById = jest.fn().mockReturnValue(false);
+
+		// Act
+		try {
+			await sut.getCommandById(0);
+		} catch (e) {
+
+			// Assert
+			expect(e instanceof InvalidRequestError).toBe(true);
+		}
+
+	});
+
+	test('should throw InvalidRequestError when getCommandById is given a of a decimal value)', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getById = jest.fn().mockReturnValue(false);
+
+		// Act
+		try {
+			await sut.getCommandById(1.01);
+		} catch (e) {
+
+			// Assert
+			expect(e instanceof InvalidRequestError).toBe(true);
+		}
+
+	});
+
+	test('should throw InvalidRequestError when getCommandById is given not a number)', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getById = jest.fn().mockReturnValue(false);
+
+		// Act
+		try {
+			await sut.getCommandById(NaN);
+		} catch (e) {
+
+			// Assert
+			expect(e instanceof InvalidRequestError).toBe(true);
+		}
+
+	});
+
+	test('should throw DataNotFoundError when getCommandById is given a valid id that is not in the database)', async () => {
+
+		// Arrange
+		expect.hasAssertions();
+		mockRepo.getById = jest.fn().mockReturnValue(false);
+
+		// Act
+		try {
+			await sut.getCommandById(1104);
+		} catch (e) {
+
+			// Assert
+			expect(e instanceof DataNotFoundError).toBe(true);
+		}
+
 	});
 
 });
