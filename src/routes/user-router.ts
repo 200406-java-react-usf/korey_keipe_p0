906712f340823c3d925650beb1fa4b44fc18539c
khkeipe <kkeipe@gmail.com>
2020-05-04 22:42:21 -0500

    adding adminGurad to endpoints

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,9 +1,30 @@
 import express from 'express';
 import Appconfig from '../config/app';
+import url from 'url';
+import { adminGuard } from '../middleware/auth-middleware';
+import { vaildateEmptyObj } from '../util/validation';
 
 export const UserRouter = express.Router();
 
 const userService = Appconfig.userService;
+
+UserRouter.get('/', adminGuard, async (req,resp) => {
+	try {
+
+		let reqURL = url.parse(req.url, true);
+
+		if(!vaildateEmptyObj(reqURL.query)) {
+			let payload = await userService.getUserByUniqueKey({...reqURL.query});
+			resp.status(200).json(payload);
+		} else {
+			let payload = await userService.getAllUsers();
+			resp.status(200).json(payload);
+		}
+	} catch (e) {
+
+		resp.status(e.statusCode).json(e);
+	}
+});
 
 UserRouter.get('/', async (req, res) => {
 	try{
@@ -34,7 +55,7 @@ UserRouter.post('', async (req, res) => {
 	}
 });
 
-UserRouter.delete('', async (req, res) => {
+UserRouter.delete('', adminGuard, async (req, res) => {
 
 	try{
 		let payload = await userService.deleteUserById(req.body);
